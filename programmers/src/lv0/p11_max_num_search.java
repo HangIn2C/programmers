53f10292092f5ab2b1731fccf5611460b6781fb3
SIDE\Side No.1 <dnjsdls321@naver.com>
2022-12-20 10:02:11 +0900

    최대 수 찾기_2

--- programmers/src/lv0/p11_max_num_search.java
+++ programmers/src/lv0/p11_max_num_search.java
@@ -2,10 +2,6 @@ package lv0;
 
 import java.util.Arrays;
 
-//정수 num과 k가 매개변수로 주어질 때,
-//num을 이루는 숫자 중에 k가 있으면 num의 그 숫자가 있는 자리 수를 return하고
-//없으면 -1을 return 하도록 solution 함수를 완성해보세요.
-
 class Solution11 {
     public int[] solution(int[] array) {
         int[] answer = new int[2]; // answer[0] : 가장 큰 숫자 / answer[1] : 숫자 위치
@@ -22,7 +18,7 @@ class Solution11 {
         return answer;
 	} // solution
 } // class
-
+ 
 public class p11_max_num_search {
 
 	public static void main(String[] args) {
