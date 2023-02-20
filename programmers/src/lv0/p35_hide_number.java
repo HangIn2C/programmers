8fe7ef4bcfad06fd96171aecf378058ab55ca774
SIDE\Side No.1 <dnjsdls321@naver.com>
2023-02-20 15:49:48 +0900

    숨어있는 숫자의 덧셈 (2)

---
+++ programmers/src/lv0/p35_hide_number.java
@@ -0,0 +1,37 @@
+package lv0;
+
+import java.util.*;
+
+//문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다.
+//my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+
+class Solution35 {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        // 숫자만 확인
+        String[] str = my_string.trim().split("[a-zA-Z]");
+        
+    	for(String s : str) {
+    		if (!s.isEmpty()) {
+    			System.out.println(s);
+    			answer += Integer.parseInt(s);
+			}
+    	} // for-each
+    	System.out.println(answer);
+        
+        return answer;
+	} // solution
+} // class
+
+public class p35_hide_number {
+
+	public static void main(String[] args) {
+		Solution35 solution = new Solution35();
+		solution.solution("aAb1B2cC34oOp");
+		solution.solution("1a2b3c4d123Z");
+		solution.solution("asdfasdvbZKJHDKFS");
+		
+	} // main
+
+} // class
